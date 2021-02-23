885a63d2065fbe7e1c6ff48264ddbbd4f91b4da3
Nflores398 <70032405+Nflores398@users.noreply.github.com>
2021-02-23 14:30:50 -0600

    Add files via upload

---
+++ Dogs.sql
@@ -0,0 +1,123 @@
+DROP TABLE Dogs;
+
+CREATE TABLE Dogs{
+ID int(4) PRIMARY KEY,
+Breed char(30) NOT NULL,
+Name char(20) NOT NULL,
+Weight int(3) NOT NULL
+};
+INSERT INTO Dogs VALUE{
+'1564',
+'Labrador Retriever',
+'Sammy',
+'54'
+};
+INSERT INTO Dogs VALUE{
+'1792',
+'Husky',
+'Daisy',
+'52'
+};
+INSERT INTO Dogs VALUE{
+'1602',
+'German Shepherd',
+'Max',
+'90'
+};
+INSERT INTO Dogs VALUE{
+'1129',
+'Great Dane',
+'Rocky',
+'150'
+};
+INSERT INTO Dogs VALUE{
+'1893',
+'Golden Retriever',
+'Goldie',
+'64'
+};
+INSERT INTO Dogs VALUE{
+'0986',
+'French Bulldog',
+'Milo',
+'26'
+};
+DESCRIBE Dog;
+SELECT * FROM Dog;
+
+CREATE TABLE Visit{
+VisitID int(8) PRIMARY KEY  AUTO_INCREMENT,
+DogID int(4) NOT NULL,
+VisitDate DATE NOT NULL,
+Duration TIME NOT NULL
+
+FOREIGN KEY (DogID) REFERENCES Dogs(ID),
+};
+
+INSERT INTO Visit
+(DogID, VisitDate, Duration)
+ VALUE{
+'1792',
+'2020-09-07',
+ '1:12:00'
+};
+INSERT INTO Visit
+(DogID, VisitDate, Duration)
+ VALUE{
+'1792',
+'2020-9-12',
+ '00:38:00'
+};
+INSERT INTO Visit
+(DogID, VisitDate, Duration)
+ VALUE{
+'0986',
+'2020-10-01',
+ '1:30:00'
+};
+INSERT INTO Visit
+(DogID, VisitDate, Duration)
+ VALUE{
+'1602',
+'2020-10-05',
+ '00:58:00'
+};
+INSERT INTO Visit
+(DogID, VisitDate, Duration)
+ VALUE{
+'1792',
+'2020-11-02',
+ '2:25:00'
+};
+INSERT INTO Visit
+(DogID, VisitDate, Duration)
+ VALUE{
+'1564',
+'2020-11-05',
+ '1:21:00'
+};
+INSERT INTO Visit
+(DogID, VisitDate, Duration)
+ VALUE{
+'1893',
+'2020-11-25',
+ '00:34:00'
+};
+INSERT INTO Visit
+(DogID, VisitDate, Duration)
+ VALUE{
+'1129',
+'2020-12-08',
+ '3:25:00'
+};
+INSERT INTO Visit
+(DogID, VisitDate, Duration)
+ VALUE{
+'1792',
+'2020-12-27',
+ '00:14:00'
+};
+DESCRIBE Visit;
+SELECT * FROM Visit;
+ALTER TABLE Visit ADD Bill float(6);
+SELECT * FROM Visit;
