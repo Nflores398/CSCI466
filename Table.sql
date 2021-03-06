9e85772324626857070889e8ec635a933c7c5719
Nflores398 <70032405+Nflores398@users.noreply.github.com>
2021-03-06 15:57:18 -0600

    Add files via upload

---
+++ Table.sql
@@ -0,0 +1,163 @@
+CREATE TABLE S(
+S char(2) PRIMARY KEY,
+SNAME char(20) NOT NULL,
+STATUS int(5) NOT NULL,
+CITY char(20) NOT NULL
+);
+CREATE TABLE P(
+P char(2) PRIMARY KEY,
+PNAME char(20) NOT NULL,
+COLOR char(10) NOT NULL,
+WEIGHT int(5) NOT NULL
+);
+CREATE TABLE SP(
+S char(2) NOT NULL,
+P char(2) NOT NULL,
+QTY int(5) NOT NULL,
+PRIMARY KEY (S),
+PRIMARY KEY (P),
+FOREIGN KEY (S) REFERENCES S(S),
+FOREIGN KEY (P) REFERENCES P(P)
+);
+INSERT INTO S
+ VALUE(
+'S1',
+'Smith',
+ '20',
+ 'London'
+);
+INSERT INTO S
+ VALUE(
+'S2',
+'Jones',
+ '10',
+ 'Paris'
+);
+INSERT INTO S
+ VALUE(
+'S3',
+'Blake',
+ '30',
+ 'Paris'
+);
+INSERT INTO S
+ VALUE(
+'S4',
+'Clark',
+ '20',
+ 'Paris'
+);
+INSERT INTO S
+ VALUE(
+'P1',
+'Nut',
+ 'Red',
+ '12'
+);
+INSERT INTO P
+ VALUE(
+'P2',
+'Bolt',
+ 'Green',
+ '17'
+);
+INSERT INTO P
+ VALUE(
+'P3',
+'Screw',
+ 'Blue',
+ '17'
+);
+INSERT INTO P
+ VALUE(
+'P4',
+'Screw',
+ 'Red',
+ '14'
+);
+INSERT INTO P
+ VALUE(
+'P5',
+'Cam',
+ 'Blue',
+ '12'
+);
+INSERT INTO P
+ VALUE(
+'P6',
+'Cog',
+ 'Red',
+ '19'
+);
+INSERT INTO SP
+ VALUE(
+'S1',
+'P1',
+ '300',
+);
+INSERT INTO P
+ VALUE(
+'S1',
+'P2',
+ '300',
+);
+INSERT INTO P
+ VALUE(
+'S1',
+'P3',
+ '400',
+);
+INSERT INTO P
+ VALUE(
+'S1',
+'P4',
+'200',
+);
+INSERT INTO P
+ VALUE(
+'S1',
+'P5',
+ '100',
+);
+INSERT INTO P
+ VALUE(
+'S1',
+'P6',
+ '100',
+);
+INSERT INTO P
+ VALUE(
+'S2',
+'P1',
+ '300',
+);
+INSERT INTO P
+ VALUE(
+'S2',
+'P2',
+ '400',
+);
+INSERT INTO P
+ VALUE(
+'S3',
+'P1',
+ '200',
+);
+INSERT INTO P
+ VALUE(
+'S4',
+'P2',
+ '200',
+);
+INSERT INTO P
+ VALUE(
+'S4',
+'P4',
+ '300',
+);
+INSERT INTO P
+ VALUE(
+'S4',
+'P5',
+ '400',
+);
